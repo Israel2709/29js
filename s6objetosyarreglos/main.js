0b5b2d0d3a4896d383e5b3d5576158908c43f39e
Israel Salinas <israel.salinas.m@gmail.com>
2023-10-20 21:46:00 -0600

    pensamiento estructurado

--- s6objetosyarreglos/main.js
+++ s6objetosyarreglos/main.js
@@ -95,3 +95,62 @@ let result2 = createNewPhrase("Ojos que no ven corazón que no siente");
 
 console.log(result);
 console.log(result2);
+
+let koders = [
+  {
+    name: "Said Barrera",
+    generation: 12,
+    score: 9.2,
+  },
+  {
+    name: "Helena Fuentes",
+    generation: 8,
+    score: 8.7,
+  },
+  {
+    name: "Gerardo Cabrera",
+    generation: 12,
+    score: 9.9,
+  },
+  {
+    name: "Brenda Guerrero",
+    generation: 8,
+    score: 8.4,
+  },
+  {
+    name: "Alejandro Fuentes",
+    generation: 7,
+    score: 8.6,
+  },
+  {
+    name: "Sandra Tellez",
+    generation: 9,
+    score: 9.9,
+  },
+];
+
+/*
+1.- Entregar el promedio general del score de los koders
+2.- Obtener una nueva lista que contenga únicamente aquellos koders cuyo score sea mayor que 9
+3.- Saber cuantos koders hay de alguna generación específica:
+      1 -> no hay koders de esta generación
+      9 -> hay 1 koder de la generación 9
+      12 -> hay 2 koders de la generación 12
+*/
+
+function countByGeneration(generation) {
+  let quantity = 0;
+  for (let i = 0; i < koders.length; i++) {
+    let currentKoder = koders[i];
+    let currentGeneration = currentKoder.generation;
+
+    if (currentGeneration === generation) {
+      quantity += 1;
+    }
+  }
+  return quantity !== 0
+    ? `Tenemos ${quantity} koders en la generación ${generation}`
+    : `No tenemos koders en esta generación`;
+}
+
+console.log(countByGeneration(12));
